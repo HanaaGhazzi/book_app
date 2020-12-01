456742012df217dea77221bb1b46567bd3596dc7
HanaaGhazzi <h.ghazzi@outlook.com>
2020-12-01 13:18:51 +0200

    lab12 is done

--- server.js
+++ server.js
@@ -14,11 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.redirect('/index')
-})
 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
     let SQL = 'SELECT * FROM books;';
   client.query(SQL)
         .then(results => {
